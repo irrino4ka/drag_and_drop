5b241f20ebef79b4bc4abcab9b9b20a9d58c514c
Iryna Yelizarova <ir.rino4ka@gmail.com>
2021-04-23 10:29:21 +0300

    added svg files

--- index.js
+++ index.js
@@ -8,11 +8,14 @@ function drag(ev) {
 
 function drop(ev) {
     ev.preventDefault();
+    const x = ev.clientX;
+    const y = ev.clientY;
     const data = ev.dataTransfer.getData("text");
     const nodeCopy = document.getElementById(data).cloneNode(true);
 
     nodeCopy.id = "newId";
     ev.target.appendChild(nodeCopy);
+    nodeCopy.setAttribute("style", "position: absolute; top: "+y+"px; left:"+x+"px;");
     // var data = ev.dataTransfer.getData("text");
     // ev.target.appendChild(document.getElementById(data));
 }
